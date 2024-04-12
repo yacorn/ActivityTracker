02a2424cf979ef8dd2c43191a86a723d7ede0f4b
akapa2002 <159844237+akapa2002@users.noreply.github.com>
2024-04-12 16:22:57 -0400

    Update popup.js
    attempt to display times on popup

--- CS321-Productivity-Tracker-main 2/popup.js
+++ CS321-Productivity-Tracker-main 2/popup.js
@@ -1,12 +1,19 @@
+<input type="submit" id="start" value="start">
+<input type="submit" id="stop" value="stop">
+
+let start = document.querySelector('StartSession');
+let stop = document.querySelector('EndSession');
 let timerID;
-document.getElementById('StartSession').addEventListener('click', function() {
+
+start.addEventListener('click', function() {
     // Send a message to the content script to start the session
     timerID = setInterval(function() {
-        console.log('!')
+        let date = new Date;
+        console.log(date.getMinutes() + ' ' + date.getSeconds());
     }, 1000);                              
 });
   
-document.getElementById('EndSession').addEventListener('click', function() {
+stop.addEventListener('click', function() {
     // Send a message to the content script to end the session
     clearInterval(timerID);
 });
