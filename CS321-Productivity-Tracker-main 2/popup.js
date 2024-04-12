0f835ead044ef12e85b52b6a0ef4dd14e1e26cfc
akapa2002 <159844237+akapa2002@users.noreply.github.com>
2024-04-12 15:45:14 -0400

    Update popup.js
    tried adding functionality to the buttons

--- CS321-Productivity-Tracker-main 2/popup.js
+++ CS321-Productivity-Tracker-main 2/popup.js
@@ -1,9 +1,14 @@
+let timerID;
 document.getElementById('StartSession').addEventListener('click', function() {
     // Send a message to the content script to start the session
+    timerID = setInterval(function() {
+        console.log('!')
+    }, 1000);                              
 });
   
 document.getElementById('EndSession').addEventListener('click', function() {
     // Send a message to the content script to end the session
+    clearInterval(timerID);
 });
   
 document.getElementById('tabInfo').addEventListener('click', function() {
